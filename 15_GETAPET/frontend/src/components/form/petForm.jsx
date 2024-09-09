9ae960dcad13f79cc3850cca69276b48cff60188
gabriel.silva <gabriel.silva@agpr5.net>
2024-09-09 13:54:52 -0300

    corrijindo pequenos erros

--- 15_GETAPET/frontend/src/components/form/petForm.jsx
+++ 15_GETAPET/frontend/src/components/form/petForm.jsx
@@ -27,7 +27,7 @@ function PetForm({ handleSubmit, petData, btnText }) {
 
     function submit(e) {
         e.preventDefault();
-        console.log(pet);
+        
         handleSubmit(pet);
     }
 
